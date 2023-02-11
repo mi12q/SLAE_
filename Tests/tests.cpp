f6dee559658d63ad5db1cbfd4d46ba8a08c8c243
milica <milicamisimovic@gmail.com>
2023-02-11 13:24:20 +0300

    added test

--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -18,11 +18,22 @@ for (int i = 1; i < x.size(); i++){
 }
 
 }
-
 TEST(test2, _4x4_matrix){
     std::vector<elements> v = {{0,7,3},{1,9,10},{2,4,4},{4,3,0}};
     std::vector<double> solution = {0.77, -1.47, 1.54, 0.94};
     std::vector<double> f = {1, 3, 7, 9};
+    Tridiagonal_matrix matrix(v);
+    std::vector<double> x = solve(matrix,f);
+    for (int i = 1; i < x.size(); i++){
+        ASSERT_NEAR(x[i], solution[i], 0.1);
+    }
+
+}
+
+TEST(test3, _6x6_matrix){
+    std::vector<elements> v = {{0,10,3},{1,9,10},{2,5,7},{6,3,1}, {7,12,9},{3,5,0}};
+    std::vector<double> solution = {12.4, 3.3, -0.81, 0.63, 11.96, -6.77};
+    std::vector<double> f = {134, 34, 7, 9, 87, 2};
     Tridiagonal_matrix matrix(v);
     std::vector<double> x = solve(matrix,f);
     for (int i = 1; i < x.size(); i++){
