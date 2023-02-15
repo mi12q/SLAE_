51277979ab1eee729f0817b50690d1cac3f1cead
milica <milicamisimovic@gmail.com>
2023-02-15 19:13:37 +0300

    added csr_tests

--- Tests/CSR_tests.cpp
+++ Tests/CSR_tests.cpp
@@ -27,7 +27,58 @@ TEST(csr_test1, _3x3_matrix){
     for (int i = 0; i < a.size(); i++){
         ASSERT_NEAR(res[i], solution[i], 0.01);
     }
+}
 
+TEST(csr_test2, all_zero_matrix){
+    std::map<std::pair<int, int>, double> v;
+    v[{0, 0}] = 0;
+    v[{0, 1}] = 0;
+    v[{0, 2}] = 0;
+    v[{1, 0}] = 0;
+    v[{1, 1}] = 0;
+    v[{1, 2}] = 0;
+    v[{2, 0}] = 0;
+    v[{2, 1}] = 0;
+    v[{2, 2}] = 0;
+
+    Matrix M(v);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            EXPECT_TRUE(M(i, j) == v.at({i,j}));
+        }
+    }
+    std::vector<double> a = {1, 2, 3};
+    std::vector<double> solution = {0, 0, 0};
+    std::vector<double> res = M.multiply(a);
+    for (int i = 0; i < a.size(); i++){
+        ASSERT_NEAR(res[i], solution[i], 0.01);
+    }
+}
+
+TEST(csr_test3, one_zero_row_matrix){
+    std::map<std::pair<int, int>, double> v;
+    v[{0, 0}] = 0;
+    v[{0, 1}] = 0;
+    v[{0, 2}] = 0;
+    v[{1, 0}] = 1;
+    v[{1, 1}] = 1;
+    v[{1, 2}] = 1;
+    v[{2, 0}] = 2;
+    v[{2, 1}] = 5;
+    v[{2, 2}] = 7;
+
+    Matrix M(v);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            EXPECT_TRUE(M(i, j) == v.at({i,j}));
+        }
+    }
+    std::vector<double> a = {1.3, 3.4, 7.5};
+    std::vector<double> solution = {0, 12.2, 72.1};
+    std::vector<double> res = M.multiply(a);
+    for (int i = 0; i < a.size(); i++){
+        ASSERT_NEAR(res[i], solution[i], 0.01);
+    }
 }
 
 int main(int argc, char **argv) {
--- src/Tridiagonal_matrix.hpp
+++ src/Tridiagonal_matrix.hpp
@@ -1,21 +1,38 @@
-#include "Tridiagonal_matrix.h"
+#ifndef SLAE_TRIDIAGONAL_MATRIX_HPP
+#define SLAE_TRIDIAGONAL_MATRIX_HPP
+
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <array>
 
 template<typename T>
-Tridiagonal_matrix<T>::Tridiagonal_matrix(const std::vector<elements<T>> &v) {
-    this->M = v;
-}
+struct elements{
+    T a; //lower diagonal element
+    T b; //main diagonal element
+    T c; //upper diagonal element
+};
 
 template<typename T>
-T Tridiagonal_matrix<T>::operator()(unsigned long long int i, unsigned long long int j) const {
-    if(j == 0)
-        return M[i].a;
-    if(j==1)
-        return M[i].b;
-    if(j==2)
-        return M[i].c;
-}
+class Tridiagonal_matrix{
+private:
+    std::vector<elements<T>> M;
+public:
+    Tridiagonal_matrix(const std::vector<elements<T>>& v){
+        this->M = v;
+    }
+    ~Tridiagonal_matrix()= default;
+    const T operator()(unsigned long long int i,unsigned long long int j) const{
+        if(j == 0)
+            return M[i].a;
+        if(j==1)
+            return M[i].b;
+        if(j==2)
+            return M[i].c;
+    }
+    unsigned long long int size() const{
+        return M.size();
+    }
+};
 
-template<typename T>
-unsigned long long int Tridiagonal_matrix<T>::size() const {
-    return M.size();
-}
+#endif //SLAE_TRIDIAGONAL_MATRIX_HPP
