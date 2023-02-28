237f8e9f43a971c63b05f5f616622ead665f1b64
milica <milicamisimovic@gmail.com>
2023-03-01 01:10:28 +0300

    got R

--- src/Householder.hpp
+++ src/Householder.hpp
@@ -25,28 +25,36 @@ Dense_matrix<T> Householder(const Dense_matrix<T> &M) {
             A.swap_element(i, j, prod[i]);
         }
     }
+
     //pravilno
 
     for (int k = 1; k < A.get_length(); k++) {
-
-        std::vector<T> v = A.get_column(k) + sign(A.get_column(k)) * mod(A.get_column(k)) * e;
+        size --;
+        std::vector<T> e(size, 0);
+        e[0] = 1;
+        std::vector<T> x;
+        std::vector<T> a  = A.get_column(k);
+        x = std::vector<T>(a.begin() + k, a.end());
+        std::vector<T> v = x + sign(x) * mod(x) * e;
         vectors.push_back(v);
-        std::vector<T> prod = A.get_column(k) - 2 * v * ((v * A.get_column(k)) / pow(mod(v), 2));
+        std::vector<T> prod = x - 2 * v * ((v * x) / pow(mod(v), 2));
         A.swap_element(k, k, prod[0]);
-        for (int i = 1 + k; i < size; i++) {
+        for (int i = 1 + k; i < size+1; i++) {
             A.swap_element(i, k, 0);
         }
 
         for (int j = 1 + k; j < A.get_length(); j++) {
-            std::vector<T> prod = A.get_column(j) - 2 * v * ((v * A.get_column(j)) / pow(mod(v), 2));
+            std::vector<T> a  = A.get_column(j);
+            x = std::vector<T>(a.begin() + k, a.end());
+            std::vector<T> prod = x- 2 * v * ((v * x) / pow(mod(v), 2));
             for (int i = 0; i < size; i++) {
                 A.swap_element(i + k, j, prod[i]);
-                return A;
             }
 
         }
 
         }
+    return A;
 
 
 }
