403493ac31d06cb87bdcd7d7e36e73556e054579
milica <milicamisimovic@gmail.com>
2023-03-01 21:57:33 +0300

    fixed Householder

--- src/Householder.hpp
+++ src/Householder.hpp
@@ -4,14 +4,8 @@
 #include "../src/Dense_matrix.hpp"
 #include <iostream>
 
-template <typename T>
-struct QR{
-    Dense_matrix<T> Q;
-    Dense_matrix<T> R;
-};
-
 template<typename T>
-struct QR<T> Householder(const Dense_matrix<T> &M) {
+std::pair<Dense_matrix<T>, Dense_matrix<T>> Householder(const Dense_matrix<T> &M) {
     Dense_matrix<T> R = M;
     int size = R.get_elements().size() / R.get_length();
     std::vector<T> e(size, 0); // vector e1
@@ -26,7 +20,6 @@ struct QR<T> Householder(const Dense_matrix<T> &M) {
     }
     q = q - 2 * transposed_prod(v)/row_column_prod(v);
     Dense_matrix<T> Q(q, R.get_length()); //calculating Q[0]
-
     std::vector<T> prod = R.get_column(0) - 2 * v * ((v * R.get_column(0)) / pow(mod(v), 2));
     R.swap_element(0, 0, prod[0]);
     for (int i = 1; i < R.get_length(); i++) {
@@ -38,7 +31,6 @@ struct QR<T> Householder(const Dense_matrix<T> &M) {
             R.swap_element(i, j, prod[i]); //transforming rest of the columns
         }
     }
-    
     for (int n = 1; n < R.get_length(); n++) { //repeating for other columns
         size--;
         std::vector<T> e(size, 0); //new vector e1
@@ -51,7 +43,6 @@ struct QR<T> Householder(const Dense_matrix<T> &M) {
         for (int i = 1 + n; i < size + 1; i++) {
             R.swap_element(i, n, 0);
         }
-
         for (int j = 1 + n; j < R.get_length(); j++) { //transforming columns after n
             std::vector<T> a = R.get_column(j);
             std::vector<T> x = std::vector<T>(a.begin() + n, a.end());
@@ -62,15 +53,14 @@ struct QR<T> Householder(const Dense_matrix<T> &M) {
         }
         for (int i = 0; i < R.get_length() ; i++) {
             a = Q.get_row(i);
-            std::vector<T> x = std::vector<T>(a.begin()+n , a.end());
-            for(int j = 0; j < size; j++){
+            std::vector<T> x = std::vector<T>(a.begin() + n, a.end());
+            for (int j = 0; j < size; j++) {
                 std::vector<T> prod = x - 2 * v * ((v * x) / pow(mod(v), 2));
-                Q.swap_element(i,j + n,prod[j]);
+                Q.swap_element(i, j + n, prod[j]);
             }
         }
     }
-    QR result = {Q,R};
-    return result;
+    return std::make_pair(Q,R);
 }
 
 #endif //SLAE__HOUSEHOLDER_HPP
