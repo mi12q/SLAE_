ca6f1d5549627d1edcea1f9b8dbee45bf20898ac
milica <milicamisimovic@gmail.com>
2023-03-06 16:48:37 +0300

    fixed_point_iteration

--- Tests/Householder.cpp
+++ Tests/Householder.cpp
@@ -34,30 +34,6 @@ TEST(qr_test2, _3x3_matrix){
 
 }
 
-//TEST(qr_test3, _10x10_matrix){
-//    std::vector<double> b(16);
-//    std::generate(b.begin(), b.end(), []() {
-//        return rand() % 5;
-//    });
-//    Dense_matrix M(b,4);
-//    Dense_matrix<double> Q =  Householder(M).first;
-//    Dense_matrix<double> R = Householder(M).second;
-//
-//    EXPECT_TRUE(unitary(Q));
-//    EXPECT_TRUE(upper_diagonal(R));
-//    Dense_matrix<double> A = Q*R;
-//    // std::cout << A.get_elements() << std::endl;
-//    //std::cout << M.get_elements();
-//    std::cout << R.get_column(0) << std::endl;
-//    std::cout << R.get_column(1) << std::endl;
-//    std::cout << R.get_column(2) << std::endl;
-//    std::cout << R.get_column(3) << std::endl;
-//    EXPECT_TRUE(A == M);
-//
-//}
-
-
-
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
