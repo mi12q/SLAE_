d49a3d2f7de1f22a42793e7b8a07b719a4cae7a3
milica <milicamisimovic@gmail.com>
2023-03-31 00:19:22 +0300

    gauss seidel fixed

--- src/iteration_methods/gauss_seidel.hpp
+++ src/iteration_methods/gauss_seidel.hpp
@@ -9,27 +9,30 @@ template <typename T>
 std::vector<T> Gauss_seidel(const Matrix<T> &A, const std::vector<T> &b, const std::vector <T> &x, T tolerance){
     std::ofstream file;
     file.open("/home/milica/CLionProjects/SLAE_/src/iteration_methods/gauss_seidel.txt");
+    std::vector<int> rows = A.get_rows();
+    std::vector<int> columns = A.get_columns();
+    std::vector<T> values = A.get_values();
     std::vector<T> x0 = x;
-    std::vector<T> xi(x.size());
-    std::vector<T> r = A.multiply(x0)-b;
+    T diag_el;
+    std::vector<T> r = A.multiply(x)-b;
     int n = 0;
     while(mod(r) > tolerance){
         for(int i = 0 ; i < x.size(); i++){
-            xi[i] = b[i];
-            for(int j = i + 1; j < x.size(); j++ ){
-                xi[i] -= A(i,j) * x0[j];
+            x0[i] = b[i];
+            for(int j = rows[i]; j < rows[i+1]; j++){
+                if (columns[j] == i){
+                    diag_el = A(i,i);
+                    continue;
+                }
+                x0[i] -= values[j]*x0[columns[j]];
             }
-            for (int j = 0; j < i; j++){
-                xi[i] -= A(i,j) * xi[j];
-            }
-        xi[i] = xi[i]/A(i,i);
+            x0[i] = x0[i]/diag_el;
         }
-        x0 = xi;
-        r = A.multiply(xi)-b;
+        r = A.multiply(x0)-b;
         file << mod(r) << " " << n << std::endl;
         n++;
     }
-    return xi;
+    return x0;
 }
 
 #endif //SLAE__GAUSS_SEIDEL_HPP
