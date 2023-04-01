bc0a33a36be0e7bbe96dc2457c1a08a8aac04045
milica <milicamisimovic@gmail.com>
2023-04-01 12:45:44 +0300

    added SOR

--- src/iteration_methods/SOR.hpp
+++ src/iteration_methods/SOR.hpp
@@ -1,29 +1,33 @@
-#ifndef SLAE__GAUSS_SEIDEL_HPP
-#define SLAE__GAUSS_SEIDEL_HPP
+#ifndef SLAE__SOR_HPP
+#define SLAE__SOR_HPP
+
 #include <iostream>
 #include <fstream>
 #include "../CSR_matrix.hpp"
 #include "../Dense_matrix.hpp"
 
 template <typename T>
-std::vector<T> Gauss_seidel(const Matrix<T> &A, const std::vector<T> &b, const std::vector <T> &x, T tolerance){
+std::vector<T> SOR(const Matrix<T> &A, const std::vector<T> &b, const std::vector <T> &x, T tolerance, T w){
     std::ofstream file;
-    file.open("/home/milica/CLionProjects/SLAE_/src/iteration_methods/gauss_seidel.txt");
+    file.open("/home/milica/CLionProjects/SLAE_/src/iteration_methods/SOR.txt");
     std::vector<T> x0 = x;
+    T t;
     T diag_el;
     std::vector<T> r = A.multiply(x)-b;
     int n = 0;
     while(mod(r) > tolerance){
         for(int i = 0 ; i < x.size(); i++){
-            x0[i] = b[i];
+            t = x0[i];
+            x0[i] = b[i]*w;
             for(int j = A.get_rows()[i]; j < A.get_rows()[i+1]; j++){
                 if (A.get_columns()[j] == i){
                     diag_el = A(i,i);
                     continue;
                 }
-                x0[i] -= A.get_values()[j]*x0[A.get_columns()[j]];
+                x0[i] -= A.get_values()[j]*x0[A.get_columns()[j]]*w;
             }
             x0[i] /= diag_el;
+            x0[i] += (1-w)*t;
         }
         r = A.multiply(x0)-b;
         file << mod(r) << " " << n << std::endl;
@@ -32,4 +36,15 @@ std::vector<T> Gauss_seidel(const Matrix<T> &A, const std::vector<T> &b, const s
     return x0;
 }
 
-#endif //SLAE__GAUSS_SEIDEL_HPP
+
+
+
+
+
+
+
+
+
+
+
+#endif //SLAE__SOR_HPP
