bbe27c519a00ae11274466704c53478ce7e6e216
milica <milicamisimovic@gmail.com>
2023-04-02 15:13:32 +0300

    added SSOR

--- Tests/Iteration_methods_tests.cpp
+++ Tests/Iteration_methods_tests.cpp
@@ -4,6 +4,7 @@
 #include "../src/iteration_methods/gauss_seidel.hpp"
 #include "../src/iteration_methods/MPI_chebyshev.hpp"
 #include "../src/iteration_methods/SOR.hpp"
+#include "../src/iteration_methods/SSOR.hpp"
 #include "gtest/gtest.h"
 
 TEST(Fixed_point_iteration, _3x3_matrix){
@@ -119,6 +120,30 @@ TEST(SOR, _3x3_matrix){
     double tolerance = pow(10,-12);
     std::vector<double> solution = {0.0804084117,0.0000194982, 0.0115891967};
     std::vector<double> result = SOR(M, b, x, tolerance,1.019055);
+    for (int i = 0; i < result.size(); i++){
+        ASSERT_NEAR(result[i], solution[i], 0.01);
+    }
+
+}
+
+TEST(SSOR, _3x3_matrix){
+    std::map<std::pair<int, int>, double> v;
+    v[{0, 0}] = 12;
+    v[{0, 1}] = 17;
+    v[{0, 2}] = 3;
+    v[{1, 0}] = 17;
+    v[{1, 1}] = 15825;
+    v[{1, 2}] = 28;
+    v[{2, 0}] = 3;
+    v[{2, 1}] = 28;
+    v[{2, 2}] = 238;
+
+    Matrix M(v);
+    std::vector<double> b = {1,2,3};
+    std::vector<double> x = {1,1,1};
+    double tolerance = pow(10,-12);
+    std::vector<double> solution = {0.0804084117,0.0000194982, 0.0115891967};
+    std::vector<double> result = SSOR(M, b, x, tolerance,1.019055, 0.076);
     for (int i = 0; i < result.size(); i++){
         ASSERT_NEAR(result[i], solution[i], 0.01);
     }
---
+++ src/iteration_methods/SSOR.hpp
@@ -0,0 +1,71 @@
+#ifndef SLAE__SSOR_HPP
+#define SLAE__SSOR_HPP
+
+#include <iostream>
+#include <fstream>
+#include "../CSR_matrix.hpp"
+#include "../Dense_matrix.hpp"
+
+template <typename T>
+std::vector<T> SOR_iteration(const Matrix<T> &A, const std::vector<T> &b, const std::vector <T> &x,T w){
+    std::vector<T> x0 = x;
+    T t;
+    T diag_el;
+
+    for(int i = 0; i < x.size(); i++){ //0.5
+        t = x0[i];
+        x0[i] = b[i]*w;
+        for(int j = A.get_rows()[i]; j < A.get_rows()[i+1]; j++){
+            if (A.get_columns()[j] == i){
+                diag_el = A(i,i);
+                continue;
+            }
+            x0[i] -= A.get_values()[j]*x0[A.get_columns()[j]]*w;
+        }
+        x0[i] /= diag_el;
+        x0[i] += (1-w)*t;
+    }
+
+    for(int i = x.size()-1; i-- > 0; i){ //1
+        t = x0[i];
+        x0[i] = b[i]*w;
+        for(int j = A.get_rows()[i]; j < A.get_rows()[i+1]; j++){
+            if (A.get_columns()[j] == i){
+                diag_el = A(i,i);
+                continue;
+            }
+            x0[i] -= A.get_values()[j]*x0[A.get_columns()[j]]*w;
+        }
+        x0[i] /= diag_el;
+        x0[i] += (1-w)*t;
+    }
+
+    return x0;
+}
+
+template <typename T>
+std::vector<T> SSOR(const Matrix<T> &A, const std::vector<T> &b, const std::vector <T> &x, T tolerance, T w, T ro){
+    std::ofstream file;
+    file.open("/home/milica/CLionProjects/SLAE_/src/iteration_methods/SSOR.txt");
+    std::vector<T> y0 = x;
+    std::vector<T> y1 = SOR_iteration(A,b,y0,w);
+    std::vector<T> t = SOR_iteration(A,b,y1,w);
+    T myu_0 = 1;
+    T myu_1 = 1/ro;
+    std::vector<T> r = A.multiply(y1)-b;
+    int n = 0;
+    while(mod(r) > tolerance){
+        y0 = (2*myu_1/ro)*t - myu_0*y0;
+        myu_0 = (2/ro)*myu_1-myu_0;
+        y0 = y0/myu_0;
+        t = SOR_iteration(A,b,y0,w);
+        y1 = y0;
+        std::swap(myu_0,myu_1);
+        r = A.multiply(y1)-b;
+        file << mod(r) << " " << n << std::endl;
+        n++;
+    }
+    return y1;
+}
+
+#endif //SLAE__SSOR_HPP
